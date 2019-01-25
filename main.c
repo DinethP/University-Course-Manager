0dffc823246b3d1c35cc6a2ced194ff5ad02c480
DinethP <dineth_perera@hotmail.com>
2019-01-25 15:48:31 +0800

    Modified main.c file

--- main.c
+++ main.c
@@ -1,26 +1,3 @@
-/**
- * ENGG1110 Problem Solving by Programming
- *
- * Project Phase 3
- *
- * I declare that the assignment here submitted is original
- * except for source material explicitly acknowledged,
- * and that the same or closely related material has not been
- * previously submitted for another course.
- * I also acknowledge that I am aware of University policy and
- * regulations on honesty in academic work, and of the disciplinary
- * guidelines and procedures applicable to breaches of such
- * policy and regulations, as contained in the website.
- *
- * University Guideline on Academic Honesty:
- *   http://www.cuhk.edu.hk/policy/academichonesty/
- *
- * Student Name : P. Dineth Perera
- * Student ID   : 1155098386
- * Class/Section: ENGG1110I
- * Date         : 04/17/2018
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
